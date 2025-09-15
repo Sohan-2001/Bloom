c5b36a93eb942abf9104701f7317cb527ddcc9e9
Sohan <sohan.karfa@gmail.com>
2025-09-15 20:15:08 +0000

    for small screen its going inside the hamburger slide thing, always even

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -62,13 +62,17 @@ export default function Header() {
     <FeedbackDialog open={isFeedbackDialogOpen} setOpen={setIsFeedbackDialogOpen} />
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 items-center">
-        <div className="mr-8 flex items-center">
-          <Link href="/" className="mr-6 flex items-center space-x-2">
+        <div className="mr-4 flex items-center">
+          <Link href="/" className="mr-4 flex items-center space-x-2">
             <span className="font-headline text-3xl font-bold text-primary tracking-wider">
               BLOOM
             </span>
           </Link>
-          <nav className="hidden items-center space-x-6 text-sm font-medium md:flex">
+          <Button variant="outline" size="sm" onClick={() => setIsUploadDialogOpen(true)}>
+            <Plus className="mr-2 h-4 w-4" />
+            Post
+          </Button>
+          <nav className="hidden items-center space-x-6 text-sm font-medium md:flex ml-6">
             {categories.map((category) => (
               <Link
                 key={category}
@@ -93,10 +97,6 @@ export default function Header() {
             <Button variant="ghost" size="sm" onClick={() => setIsFeedbackDialogOpen(true)}>
                 <MessageSquareText className="mr-2 h-4 w-4" />
                 Feedback
-            </Button>
-            <Button variant="outline" size="sm" onClick={() => setIsUploadDialogOpen(true)}>
-                <Plus className="mr-2 h-4 w-4" />
-                Post
             </Button>
           </div>
 
@@ -186,10 +186,6 @@ export default function Header() {
                 </span>
               </Link>
               <div className="mt-4 flex flex-col space-y-2">
-                 <Button className="w-full" variant="outline" size="sm" onClick={() => setIsUploadDialogOpen(true)}>
-                    <Plus className="mr-2 h-4 w-4" />
-                    Post
-                </Button>
                 <Button className="w-full" variant="ghost" size="sm" onClick={() => setIsFeedbackDialogOpen(true)}>
                     <MessageSquareText className="mr-2 h-4 w-4" />
                     Feedback
